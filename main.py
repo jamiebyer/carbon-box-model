5c3d45627c11cc281b0f5933d161501b83d4d933
Jamie Byer <byerjamie@gmail.com>
2022-09-29 22:49:39 -0700

    change c flux to sinusoid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from emissions import emissions
 from func_rk4 import rk4
+from euler_method import euler_method
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,31 +10,34 @@ import time
 
 # STEP 0.1
 # Initial values in boxes in Gt
-M1_init = 725
-M2_init = 725
-M5_init = 110
-M7_init = 60
+M1 = 725
+M2 = 725
+M5 = 110
+M7 = 60
 # Array of initial masses for each box.
-M_init = np.array([M1_init, M2_init, M5_init, M7_init])
+M_init = np.array([M1, M2, M5, M7])
 
 # Fluxes in Gt/yr (Fab is the flux from box a to box b)
-F12_init = 90
-F21_init = 90
-F71_init = 55
-F57_init = 55
-F15_init = 110
-F51_init = 55
-F72_init = 0
+F12 = 90
+F21 = 90
+F71 = 55
+F57 = 55
+F15 = 110
+F51 = 55
+F72 = 0
+
+F11, F22, F52, F25, F55, F75, F17, F27, F77 = np.zeros(9)
+
+# F matrix
+F_init = [
+    [F11, F21, F51, F71],
+    [F12, F22, F52, F72],
+    [F15, F25, F55, F75],
+    [F17, F27, F57, F77],
+]
 
 # Get rate coefficients from steady-state flux and initial mass
-k12 = F12_init/M1_init
-k21 = F21_init/M2_init
-k71 = F71_init/M7_init
-k57 = F57_init/M5_init
-k15 = F15_init/M1_init
-k51 = F51_init/M5_init
-k72 = F72_init/M2_init
-
+k = np.divide(F_init, M_init)
 
 # Function used integrators
 def dMdt(t, M):
@@ -46,17 +50,17 @@ def dMdt(t, M):
     M1, M2, M5, M7 = M
 
     # Update values for fluxes from mass.
-    F12 = k12*M1
-    F21 = k21*M2
-    F71 = k71*M7
-    F57 = k57*M5
-    F15 = k15*M1
-    F51 = k51*M5
-    F72 = k72*M7
+    F = np.multiply(k, M)
+
+    F_in = np.sum(F, axis=1)
+    F_out = np.sum(F, axis=0)
+
+    dMdt = F_in - F_out
 
     # From step 0.1, add an arbitraty flux C (in Gt/yr)
     if add_flux_C:
-        C = 15
+        T = 0.1/(2*np.pi)
+        C = (20/T)*(np.sin(t/T))
     else:
         C = 0
 
@@ -65,13 +69,7 @@ def dMdt(t, M):
     else:
         e = 0
 
-    # dMdt = flux in - flux out
-    dM1dt = (F21 + F51 + F71 + C + e) - (F12 + F15) # Add emissions to the atmosphere as part of flux in
-    dM2dt = (F12 + F72) - (F21)
-    dM5dt = (F15) - (F51 + F57)
-    dM7dt = (F57) - (F71 + F72)
-
-    dMdt = np.array([dM1dt, dM2dt, dM5dt, dM7dt])
+    dMdt[0] += C + e
     
     return dMdt
 
@@ -97,7 +95,11 @@ def plot_integrator_results(title_string):
     
     t5 = time.time()
     lsoda_sol = solve_ivp(fun=dMdt, t_span=(t_min, t_max), y0=M_init, method="LSODA", max_step=max_step)
+    
     t6 = time.time()
+    t_euler, M_euler = euler_method(fxy=dMdt, x0=t_min, xf=t_max, y0=M_init, N=n)
+    t7 = time.time()
+
 
     # Get time taken for each integrator to perform integration.
     deltat_rk4 = t2-t1
@@ -105,12 +107,13 @@ def plot_integrator_results(title_string):
     deltat_rk45 = t4-t3
     deltat_dop853 = t5-t4
     deltat_lsoda = t6-t5
+    deltat_euler = t7-t6
 
     # Arrays to loop over and plot each integration type.
-    all_t = [t_rk4, rk23_sol.t, rk45_sol.t, dop853_sol.t, lsoda_sol.t]
-    all_M = [M_rk4.real, rk23_sol.y.T, rk45_sol.y.T, dop853_sol.y.T, lsoda_sol.y.T]
-    deltat = [deltat_rk4, deltat_rk23, deltat_rk45, deltat_dop853, deltat_lsoda]
-    plot_types = ["(Given) RK4", "RK23", "RK45", "DOP853", "LSODA"]
+    all_t = [t_rk4, rk23_sol.t, rk45_sol.t, dop853_sol.t, lsoda_sol.t, t_euler]
+    all_M = [M_rk4.real, rk23_sol.y.T, rk45_sol.y.T, dop853_sol.y.T, lsoda_sol.y.T, M_euler]
+    deltat = [deltat_rk4, deltat_rk23, deltat_rk45, deltat_dop853, deltat_lsoda, deltat_euler]
+    plot_types = ["(Given) RK4", "RK23", "RK45", "DOP853", "LSODA", "Euler"]
 
     # Plotting    
     fig, ax = plt.subplots()
@@ -121,10 +124,6 @@ def plot_integrator_results(title_string):
         plt.xlabel('Time (yr)')
         plt.ylabel('Mass (Gt)')
         plt.title(plot_types[ii] + ", delta t = " + "{:.2E}".format(deltat[ii]) + "s")
-
-        #plt.axvline(x=1990)
-        #plt.axvline(x=2100)
-        #plt.xlim([1800, 2200])
     
     plt.suptitle(title_string)
     fig.legend(['atmosphere', 'surface water', 'short-lived biota', 'litter'], loc="lower right")
@@ -137,13 +136,13 @@ def plot_integrator_results(title_string):
 
 #add_flux_C = True
 add_flux_C = False
-add_emissions = True
-#add_emissions = False
+#add_emissions = True
+add_emissions = False
 
 title_string = "4 box model"
 
 if add_flux_C:
-    title_string += ", add flux C = 15 Gt/yr"
+    title_string += ", add sinusoidal flux C"
 if add_emissions:
     title_string += ", add emissions"
 
